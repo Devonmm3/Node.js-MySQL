7159f5b4c04d932d6479cfb22e7c0938947c9f55
DevonMM <Devon.m.moran@gmail.com>
2019-01-31 15:51:31 -0500

    first

---
+++ bamazonCommands.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Bamazon;
+USE Bamazon;
+CREATE TABLE Products (ItemID int AUTO_INCREMENT, ProductName varchar(50) NOT NULL, DepartmentName varchar(50) NOT NULL, Price varchar(30) NOT NULL, PRIMARY KEY(ItemID) );
+describe Products;
+SELECT * FROM Products;
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+
+SELECT * FROM Products; 
