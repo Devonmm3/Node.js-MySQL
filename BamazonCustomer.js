1e45d98c557b0c923b88fd6fc782cd659f9f2677
DevonMM <Devon.m.moran@gmail.com>
2019-02-01 21:23:08 -0500

    getting module error

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -9,118 +9,120 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
+    if (err) {
+        console.log("Error connecting to Database");
+        return;
+    }
+    console.log("Connection Established");
+
+    var sqlFile = {
+        properties: {
+            ID: {
+                message: "Enter the product ID of the product you would like to purchase.",
+                pattern: /^[0-9] [0-9]$|^[0-9]$/,
+                required: true
+            },
+            howMany: {
+                message: "Please enter the nuber of items you would like to buy.",
+                pattern: /^[0-9] [0-9]$|^[0-9] [0-9] [0-9]$/,
+                required: true
+            }
+        }
+
+    };
+    var sqlFile2 = {
+        properties: {
+            SecondPurchase: {
+                message: "Would you like to purchase another item today?.",
+                pattern: /(no|n|yes|y)/,
+                required: true
+            }
+        }
+    };
+
+    var endApp = function () {
+        return next(err);
+    }
+
+    var startApp = function () {
+        connection.query("SELECT * FROM Products", function (err, result) {
+            if (err) throw err;
+            return (pullBamazonProducts(result));
+        });
+    }
+
+    var pullBamazonProducts = function (products) {
+        console.log("Hey there! Welcome to Bamazon where you can buy what ever you need to make your wishes come true! Listed we have the products, their costs and the current amount in stock.");
+        for (var i = 0; i < products.length; i++) {
+            var productsResults = "\n" +
+                "ItemID: " + products[i].ProductID + "\n" +
+                "Product Description: " + products[i].ProductName + "\n" +
+                "Product Department: " + products[i].DepartmentName + "\n" +
+                "Price: $ " + products[i].Price + "\n" +
+                "Currently in Stock: " + products[i].StockQuantity;
+            console.log(productsResults);
+        }
+
+        shopperSelectProID();
+
+    }
+
+    var shopperSelectProID = function () {
+        prompt.start();
+        console.log("Enter the Item ID for the product you would like to buy.");
+
+        prompt.get(sqlFile, function (err, result) {
             if (err) {
-                console.log("Error connecting to Db");
-                return;
+                console.log(err)
             }
-            console.log("Connection Established");
+            var shopperIdChoice = parseIn(result.ID);
+            var shopperQuantityChoice = parseInt(result.howMany);
 
-            var sqlFile = {
-                properties: {
-                    ID: {
-                        message: "Enter the product ID of the product you would like to purchase.",
-                        pattern: /^[0-9] [0-9]$|^[0-9]$/,
-                        required: true
-                    },
-                    howMany: {
-                        message: "Please enter the nuber of items you would like to buy.",
-                        pattern: /^[0-9] [0-9]$|^[0-9] [0-9] [0-9]$/,
-                        required: true
-                    }
-                }
-
-            };
-            var sqlFile2 = {
-                properties: {
-                    purchase: {
-                        message: "Would you like to purchase another item today?.",
-                        pattern: /(no|n|yes|y)/,
-                        required: true
-                    }
-                }
-            };
-
-            var endApp = function () {
-                return next(err);
-            }
-
-            var startApp = function () {
-                connection.query("SELECT * FROM Products", function (err, results) {
+            var getInventory = function () {
+                connection.query("SELECT * FROM Products WHERE ItemID =" + shopperIdChoice, function (err, result) {
                     if (err) throw err;
-                    return (pullBamazonProducts(results));
-                });
-            }
 
-            var pullBamazonProducts = function (products) {
-                console.log("Hey there! Welcome to Bamazon where you can buy what ever you need to make your wishes come true! Listed we have the products, their costs and the current amount in stock.");
-                for (var i = 0; i < products.length; i++) {
-                    var resultsProduct = "\r\n" +
-                        "ItemID: " + products[i].ItemID + "\r\n" +
-                        "Product Description: " + products[i].ProductName + "\r\n" +
-                        "Product Department: " + products[i].DepartmentName + "\r\n" +
-                        "Price: $ " + products[i].Price + "\r\n" +
-                        "Currently in Stock: " + products[i].StockQuantity;
-                    console.log(productsResults);
-                }
+                    var userAttemptsBuy = shopperQuantityChoice;
+                    var productInventory = result[0].StockQuantity;
+                    var itemsPrice = result[0].Price;
+                    var hasInStock = productInventory - userAttemptsBuy;
+                    var costTotal = itemsPrice * userAttemptsBuy;
 
-                shopperSelectProID();
-
-            }
-
-            var shopperSelectProID = function () {
-                prompt.start();
-                console.log("Enter the Item ID for the product you would like to buy.");
-
-                prompt.get(sqlFile, function (err, results) {
-                    if (err) {
-                        console.log(err)
-                    }
-                    var shopperIdChoice = parseIn(result.ID);
-                    var shopperQuantityChoice = parseInt(result.howMany);
-
-                    var getInventory = function () {
-                        connection.query("SELECT * FROM Products WHERE ItemID =" + shopperIdChoice, function (err, result) {
+                    if (userAttemptsBuy > productInventory || productInventory === 0) {
+                        console.log("We do not have enough in stock to complete this order today." + "\n" + "\n");
+                        shopperIdChoice();
+                    } else {
+                        console.log("We have " + result[0].StockQuantity + " of " + result[0].ProductName);
+                        console.log("You are buying " + userAttemptsBuy + " " + result[0].ProductName + "s at $" + result[0].Price + " per item.");
+                        console.log("Your total cost today is $" + costTotal);
+                        connection.query("UPDATE Products SET StockQuantity = " + hasInStock + " WHERE ProductID =" + shopperIdChoice, function (err, result) {
                             if (err) throw err;
+                            connection.query("SELECT ProductID, ProductName, DepartmentName, Price, StockQuantity FROM products WHERE ProductID =" + shopperIdChoice, function (err, result) {
+                                console.log(result);
+                            });
 
-                            var userAttemptsBuy = shopperQuantityChoice;
-                            var productInventory = result[0].StockQuantity;
-                            var itemPrice = result[0].Price;
-                            var hasInStock = productInventory - userAttemptsBuy;
-                            var costTotal = productsPrice * userAttemptsBuy;
+                        });
+                        prompt.get(sqlFile2, function (err, result) {
+                            if (err) {
+                                console.log(err)
+                            }
+                            console.log(result);
 
-                            if (userAttemptsBuy > productInventory || productInventory === 0) {
-                                console.log("We do not have enough in stock to complete this order today." + "\r\n" + "\r\n");
-                                shopperIdChoice();
+                            var shopperAnswer = result.purchase;
+                            if (shopperAnswer === "n" || shopperAnswer === "no") {
+                                endApp();
                             } else {
-                                console.log("We have " + result[0].StockQuantity + " of " + result[0].ProductName);
-                                console.log("You are buying " + userAttemptsBuy + " " + result[0].ProductName + "s at $" + result[0].Price + " per item.");
-                                console.log("Your total cost today is $" + costTotal);
-                                connection.query("UPDATE Products SET StockQuantity = " + hasInStock + " WHERE ItemID =" + shopperIdChoice, function (err, result) {
-                                    if (err) throw err;
-                                    connection.query("SELECT ItemID, ProductName, DepartmentName, Price, StockQuantity FROM products WHERE ItemID =" + shopperIdChoice, function (err, result) {
-
-                                    });
-
-                                });
-                                prompt.get(sqlFile2, function (err, result) {
-                                    if (err) {
-                                        console.log(err)
-                                    }
-                                    console.log(result);
-
-                                    var shopperAnswer = result.purchase;
-                                    if (shopperAnswer === "n" || shopperAnswer === "no") {
-                                        endApp();
-                                    } else {
-                                        startApp();
-                                    }
-                                });
+                                startApp();
                             }
                         });
-
                     }
-
                 });
             }
 
-            inApp();
+            getInventory();
+
+        });
+    }
+
+    startApp();
+});
--- bamazonCommands.sql
+++ bamazonCommands.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE Bamazon;
 USE Bamazon;
-CREATE TABLE Products (ItemID int AUTO_INCREMENT, ProductName varchar(50) NOT NULL, DepartmentName varchar(50) NOT NULL, Price varchar(30) NOT NULL, PRIMARY KEY(ItemID) );
+CREATE TABLE Products (ProductID int AUTO_INCREMENT, ProductName varchar(50) NOT NULL, DepartmentName varchar(50) NOT NULL, Price varchar(30) NOT NULL, PRIMARY KEY(ItemID) );
 describe Products;
 SELECT * FROM Products;
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
-INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity) VALUES ();
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (1, "Crockpot", "Home-Kitchenware", 85.89, 202);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (2, "Children's Bike", "Toys, Games and sports", 99.99, 74);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (3, "Christmas Candle", "Holiday Decorations", 15.49, 444);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (4, "Lion's Mane", "Vitamens and Health", 39.99, 376);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (5, "Temperpedic Pillow", "Home and Bedroom", 79.39, 2000);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (6, "Cards Against Humanity", "Toys, Games and sports", 14.59, 550);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (7, "Record Player", "Electronics", 79.99, 47);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (8, "Wheelbarrow", "Home and Garden", 45.69, 300);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (9, "Ice cream maker", "Home-Kitchenware", 36.77, 430);
+INSERT INTO Products (ProductID, ProductName, DepartmentName, Price, StockQuantity) VALUES (10, "Harry Potter and the Half-blood Prince", "Books", 29.99, 98);
 
 SELECT * FROM Products; 
